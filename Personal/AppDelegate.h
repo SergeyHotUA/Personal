dd2befc835abf79ac5a0c69bcb6e2ca026d7f6ca
Home <sergey@sergeys-mac-mini.local>
2017-01-23 00:00:29 +0200

    Initial Commit

---
+++ Personal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Personal
+//
+//  Created by Home on 1/23/17.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Personal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Personal
+//
+//  Created by Home on 1/23/17.
+//  Copyright © 2017 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
